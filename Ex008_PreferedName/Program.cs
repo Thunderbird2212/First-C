ce7c014be7df52242858b0c5143fe8b9d8cef407
Kirill Romanov <freibird22@gmail.com>
2022-07-08 18:49:19 +0300

    Ex008 Prefered Name project added

---
+++ Ex008_PreferedName/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maria")
+{
+    Console.WriteLine("I love you Maria!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
